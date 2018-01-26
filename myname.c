7077f2d81a1e1b1dd86814b6219b4cb80bc23ffd
dandrews7 <35712108+dandrews7@users.noreply.github.com>
2018-01-26 15:26:50 -0600

    Add files via upload

---
+++ myname.c
@@ -0,0 +1,12 @@
+/**
+  Author: Daniel Andrews
+  Date: 01/26/2018
+  Hello World- Name Replacement in C 
+*/ 
+
+#include <stdio.h> 
+ 
+int main(int argc, char **argv) {
+  printf("My name is Daniel Andrews.\n");
+  return 0;
+}
